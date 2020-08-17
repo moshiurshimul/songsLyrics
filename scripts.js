2181142e294560b2d29855a9d80af976310e1c46
moshiurshimul <hello@moshiurshimul.com>
2020-08-17 19:56:41 +0600

    cover added

--- scripts.js
+++ scripts.js
@@ -13,11 +13,15 @@ document.getElementById('search-btn').addEventListener('click', function() {
             //Song details
             const songTitle = element.title
             const albumTitle = element.album.title
+            const albumPhoto = element.album.cover_medium
 
             let showList = document.createElement('div')
             showList.className = 'single-result row align-items-center my-3 p-3'
             showList.innerHTML = 
-            `<div class="col-md-9">
+            `<div class="col-md-3">
+            <img src="${albumPhoto}" alt=""> 
+            </div>
+            <div class="col-md-6">
             <h3 class="lyrics-name">${songTitle}</h3>
             <p class="author lead">Album by <span>${albumTitle}</span></p>
             </div>
